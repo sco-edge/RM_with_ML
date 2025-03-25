35303240b272d5dffbc938cb655d84cd1e96f1cd
ksh6283 <huni6282@naver.com>
2025-03-25 15:09:33 +0900

    update deployment

--- deploy_hote_reserv/profiling.py
+++ deploy_hote_reserv/profiling.py
@@ -2,6 +2,7 @@ from testing.testCollection import full_init, start_test
 from configs import app
 
 ports = {"social": 30628, "media": 30092, "hotel": 0}
+app= "hotel"
 full_init(app, ports[app])
 
 # continues=True: Program will append to the end of existing data
